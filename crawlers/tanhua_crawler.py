a3d2df2ffbc7e8e415ddbf8a3974c512894242c9
new <2185537233@qq.com>
2026-07-25 12:14:58 +0800

    chore(tanhua): 将默认爬取结束页从 70 调整为 50
    减少 TanhuaCrawler 的默认爬取范围，从 70 页降至 50 页，
    以降低爬取负载和存储开销。

--- crawlers/tanhua_crawler.py
+++ crawlers/tanhua_crawler.py
@@ -7,7 +7,7 @@ from crawlers.base_crawler import DecryptSiteBaseCrawler, CrawlConfig
 
 class TanhuaCrawler(DecryptSiteBaseCrawler):
     CATEGORIES = ["guochan", "oumei"]
-    default_end_page = 70
+    default_end_page = 50
     default_workers = 8
 
     def __init__(self, db_manager):
